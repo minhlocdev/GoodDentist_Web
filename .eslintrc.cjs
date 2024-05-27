2b7974b0610ecce00cec28190ec5409f7c047978
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2024-05-27 13:33:45 +0700

    add radix library + config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,35 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
-  ignorePatterns: [
-    "dist",
-    ".eslintrc.cjs",
-    "postcss.config.js",
-    "tailwind.config.js",
-    "prettier.config.ts",
-    "vitest.config.ts"
-  ], // Add postcss.config.js here
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ["react-refresh"],
-  rules: {
-    "no-mixed-spaces-and-tabs": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/stylistic-type-checked'
     ],
-  },
+    ignorePatterns: [
+        'dist',
+        '.eslintrc.cjs',
+        'postcss.config.js',
+        'tailwind.config.js',
+        'prettier.config.ts',
+        'vitest.config.ts'
+    ], // Add postcss.config.js here
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname
+    },
+    plugins: ['react-refresh'],
+    rules: {
+        'no-mixed-spaces-and-tabs': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
 };
