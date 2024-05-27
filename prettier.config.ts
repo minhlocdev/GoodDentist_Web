344c419aa2c0ff52752a1a0467da7c740c9c04f7
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2024-05-27 14:14:38 +0700

    change tailwind.config.js to ts

--- prettier.config.ts
+++ prettier.config.ts
@@ -2,7 +2,7 @@ import type { Options } from "prettier";
 
 const config: Options = {
   plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.js",
+  tailwindConfig: "./tailwind.config.ts",
   tailwindAttributes: ["myClassList"],
   tailwindFunctions: ["tw"],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <div>
-      
-    <h1 className="text-3xl font-bold text-green-500 underline">
-      Hello world!
-    </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-green-500 underline">Hello world!</h1>
+        </div>
+    );
 }
 
 export default App;
