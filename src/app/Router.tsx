23d54cea0119a355fe3859e641179e88948d4cfb
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2024-06-07 08:36:07 +0700

    remove dark mode

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,56 +1,31 @@
-import React, { ComponentType, LazyExoticComponent } from 'react';
-import {
-    Route,
-    RouterProvider,
-    createBrowserRouter,
-    createRoutesFromElements
-} from 'react-router-dom';
-import ProtectedRoute from '../components/HOC/ProtectedRoute';
-import routerMeta, { IRouterMeta } from '../lib/routerMeta';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import DashboardPage from './pages/dashboard/dashboard';
 import ErrorPage from './pages/errors/error-page';
 import Layout from './pages/layout';
+import ServicePage from './pages/service/service';
+import StaffPage from './pages/staff/staff';
 
-interface ModuleType {
-    default: ComponentType<any>;
-}
-
-const lazyImport = (pageName: string): LazyExoticComponent<ComponentType<any>> => {
-    return React.lazy(() =>
-        import(`./pages/${pageName.toLowerCase()}/${pageName.toLowerCase()}`).then(
-            (module): ModuleType => ({
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                default: module.default
-            })
-        )
-    );
-};
-
-// Dynamically create routes based on routerMeta
-const routes = Object.keys(routerMeta).map((key) => {
-    const meta: IRouterMeta = routerMeta[key];
-    const Component = lazyImport(key);
-
-    return (
-        <Route
-            key={meta.path}
-            path={meta.path}
-            element={
-                <ProtectedRoute path={meta.path}>
-                    <Component />
-                </ProtectedRoute>
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: 'dashboard',
+                element: <DashboardPage />
+            },
+            {
+                path: 'services',
+                element: <ServicePage />
+            },
+            {
+                path: 'staffs',
+                element: <StaffPage />
             }
-        />
-    );
-});
-
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
-            {routes}
-        </Route>
-    )
-);
-
+        ]
+    }
+]);
 const Router = () => {
     return <RouterProvider router={router} />;
 };
--- src/app/pages/staff/staff.tsx
+++ src/app/pages/staff/staff.tsx
@@ -1,6 +1,6 @@
 import { ContentLayout } from '../../../components/admin-panel/content-layout';
 
-const ServicePage = () => {
+const StaffPage = () => {
     return (
         <ContentLayout title="Staff Management">
             <h1 className="text-secondary-foreground">Hello Staff</h1>
@@ -8,4 +8,4 @@ const ServicePage = () => {
     );
 };
 
-export default ServicePage;
+export default StaffPage;
