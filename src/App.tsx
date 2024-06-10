6614f21d7e28ffb2a90a6ad64238e316c259921f
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2024-06-10 10:21:51 +0700

    test: change router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './app/App.css';
 import Router from './Router';
 
 function App() {
-    //TODO: Router
     return <Router />;
 }
 
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,21 +1,45 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import routerMeta from './lib/routerMeta';
 import ErrorPage from './app/pages/errors/error-page';
 import Layout from './app/pages/layout';
+import StaffPage from './app/pages/staff/staff';
+// import routerMeta from './lib/routerMeta';
+import CustomerPage from './app/pages/customer/customer';
+import AppointmentPage from './app/pages/appoinment/appointment';
+import PendingAppointment from './app/pages/appoinment/pending-appointment';
 
-
-const routes = Object.values(routerMeta).map((route) => ({
-    path: route.path,
-    element: route.element,
-    errorElement: <ErrorPage />
-}));
+// const routes = Object.values(routerMeta).map((route) => ({
+//     path: route.path,
+//     element: route.element,
+//     errorElement: <ErrorPage />
+// }));
 
 const router = createBrowserRouter([
     {
         path: '',
         element: <Layout />,
         errorElement: <ErrorPage />,
-        children: routes
+        children: [
+            {
+                path: '/staffs',
+                element: <StaffPage />,
+                errorElement: <ErrorPage />
+            },
+            {
+                path: '/customers',
+                element: <CustomerPage />,
+                errorElement: <ErrorPage />
+            },
+            {
+                path: '/calendars',
+                element: <AppointmentPage />,
+                errorElement: <ErrorPage />
+            },
+            {
+                path: '/pending-appointment',
+                element: <PendingAppointment />,
+                errorElement: <ErrorPage />
+            }
+        ]
     }
 ]);
 
