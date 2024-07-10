dde58a48a883fbdc5efbda85d179ae1373cbfe4a
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2024-07-10 18:36:07 +0700

    Update: Reset Query after mutation

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Toaster } from './components/ui/toast.tsx';
 import './index.css';
+import { queryClient } from './lib/queryClient.ts';
 import AuthProvider from './providers/AuthProvider.tsx';
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            refetchOnMount: false,
-            refetchOnReconnect: false,
-            retry: false,
-            staleTime: 5 * 60 * 1000
-        }
-    }
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
