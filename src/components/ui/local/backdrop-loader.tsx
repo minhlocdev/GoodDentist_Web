47a58dd183bd427f4ec97be7c75a55d7cac749a5
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2024-07-13 18:22:16 +0700

    fix: Backdrop loader

--- src/components/ui/local/backdrop-loader.tsx
+++ src/components/ui/local/backdrop-loader.tsx
@@ -2,10 +2,10 @@ import { LoaderCircle } from 'lucide-react';
 
 const BackdropLoader = () => {
     return (
-        <div className='flex items-center justify-center first-letter:w-sceen h-screen bg-white z-40'>
+        <div className='flex items-center justify-center w-screen h-screen bg-white -z-0'>
             <LoaderCircle className="h-5 w-5 animate-spin text-neutral-500" />
         </div>
-    );
+    );  
 };
 
 export default BackdropLoader;
